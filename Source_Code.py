fc984b21919358607424615606a636df01ab21d4
Danny <64541739+Tho100@users.noreply.github.com>
2022-03-22 21:30:52 +0800

    Update Source_Code.py

--- Source_Code.py
+++ Source_Code.py
@@ -13,15 +13,17 @@ def function():
         GREET = "GREET"
         COMPLIMANT = "COMPLIMANT"
         PERIOD = "PERIOD"
-
+        QUESTION = "QUESTION"
     inputs = str(entry.get())
 
     train_x = ['Hi bro','Hello sir','Whatsupp',
-              'Nice job','Goodjob dude','Youre nice',
-              'Morning','Morning you','Good morning','Goodnight','Its evening already']
+              'Nice job','Goodjob dude','Youre nice','You did good',
+              'Morning','Morning you','Good morning','Goodnight','Its evening already',
+              'Why','Are you fine bro','Where is',"Why cant't",'Is that a','What','What do you']
     train_y = [word_category.GREET,word_category.GREET,word_category.GREET,
-              word_category.COMPLIMANT,word_category.COMPLIMANT,word_category.COMPLIMANT,
-              word_category.PERIOD,word_category.PERIOD,word_category.PERIOD,word_category.PERIOD,word_category.PERIOD,]
+              word_category.COMPLIMANT,word_category.COMPLIMANT,word_category.COMPLIMANT,word_category.COMPLIMANT,
+              word_category.PERIOD,word_category.PERIOD,word_category.PERIOD,word_category.PERIOD,word_category.PERIOD,
+              word_category.QUESTION,word_category.QUESTION,word_category.QUESTION,word_category.QUESTION,word_category.QUESTION,word_category.QUESTION,word_category.QUESTION]
     
     from sklearn.feature_extraction.text import CountVectorizer
 
@@ -36,10 +38,17 @@ def function():
     test_x = vectorizer.transform([inputs])
     prediction = clf_svm.predict(test_x)
 
-    label = tk.Label(window, text=prediction)
-    label.pack()
+    function.label = tk.Label(window, text=prediction)
+    function.label.pack()
 
+def clear_func():
+    entry.delete(0,'end')   
+    function.label.pack_forget()
+    
 button = tk.Button(window,text='Show Category',command=function)
 button.pack()
+
+clear = tk.Button(window, text='Clear',command=clear_func)
+clear.pack() 
 
 window.mainloop()
